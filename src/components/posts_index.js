f562ccb6f881cb385a37f9fb24d98db9184163f0
darren <764190362@qq.com>
2016-06-20 10:58:14 +0800

    finish11

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -7,8 +7,18 @@ import { Link } from 'react-router';
 class PostsIndex extends Component{
     
     componentWillMount(){
-//        console.log('this would be a good time to call an action creator to fetch posts');
         this.props.fetchPosts();
+    }
+    
+    renderPosts(){
+        return this.props.posts.map((post) =>{
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <span className="pull-xs-right">{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            );
+        });
     }
     
     render(){
@@ -19,16 +29,19 @@ class PostsIndex extends Component{
                         Add a Post
                     </Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-//function mapDispatchToProps(dispatch){
-//    return bindActionCreators({ fetchPosts }, dispatch);
-//}
+function mapStateToProps(state){
+    return { posts: state.posts.all }
+}
 
-//export default connect(null, mapDispatchToProps)(PostsIndex);
 
-export default connect(null, {fetchPosts:fetchPosts})(PostsIndex);
+
+export default connect(mapStateToProps, {fetchPosts:fetchPosts})(PostsIndex);
